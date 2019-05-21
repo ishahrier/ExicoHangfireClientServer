568b0636ac916153619cbe6c39bff032a3c2bcca
shahrier <ronnie.nsu@outlook.com>
2019-05-21 13:46:04 -0500

    -'..'

---
+++ Common/Managers/JobManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exico.HF.Common.Interfaces;
+using Hangfire;
+
+namespace Exico.HF.Common.Managers
+{
+    public class JobManager
+    {
+        public void CreateFireAndForgetJob(IFireAndForgetTaskOptions options, IBackgroundJobClient hfClient)
+        {
+            hfClient.Enqueue<IFireAndForgetTask>(x => x.Run(options.ToJson(), JobCancellationToken.Null));
+        }
+    }
+}
